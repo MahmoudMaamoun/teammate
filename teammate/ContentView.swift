bdb347ddcfff7587aedcade192a58d54f3db88c3
Mahmoud Maamoun <maamounsharaky@gmail.com>
2025-07-06 02:57:31 +0300

    Initial Commit

---
+++ teammate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  teammate
+//
+//  Created by Mahmoud Maamoun on 06/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ teammate/teammateApp.swift
@@ -0,0 +1,17 @@
+//
+//  teammateApp.swift
+//  teammate
+//
+//  Created by Mahmoud Maamoun on 06/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct teammateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
